7039908eb0255ee6a2ac5f2a102f7d8950787282
nateowl <55293968+nateowl@users.noreply.github.com>
2020-01-24 11:33:37 -0500

    Basic testing for game change

--- script/app.js
+++ script/app.js
@@ -1,4 +1,14 @@
 (() => {
 	// set up the puzzle pieces and boards
-	
+	// buttons at the bottom of the page
+	const navbuttons = document.querySelectorAll('#buttonHolder img');
+
+	function chanegImageSet() {
+		//Change the avalable pieces and the background of dropzone.
+
+		debugger;
+	}
+
+	navbuttons.forEach(button => button.addEventListener('click', chanegImageSet));
+
 })();
